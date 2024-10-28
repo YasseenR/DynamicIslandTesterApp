5c03512d92d41914a425d7b0c5c91d0a36bf012c
YasseenR <yasseenrouni@gmail.com>
2024-10-28 09:47:31 -0400

    Initial Commit

---
+++ DynamicIslandTestApp/DynamicIslandTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandTestAppApp.swift
+//  DynamicIslandTestApp
+//
+//  Created by Yasseen Rouni on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
